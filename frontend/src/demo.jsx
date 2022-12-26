fa120204e862261cf1b8c556f4541e9fb54f5fdc
GZZ <gzz_2000@126.com>
2022-12-26 12:34:55 +0800

    add frontend skeleton

---
+++ frontend/src/demo.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './index.css';
+import { Breadcrumb, Layout, Menu, theme, Input } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+const App: React.FC = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+  return (
+    <Layout className="layout">
+      <Header>
+        <div className="logo"></div>
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['home']}
+          items={[
+            {key: 'home', label: 'Home'}
+          ]}
+        />
+      </Header>
+      <Content style={{ padding: '0 50px' }}>
+        <Breadcrumb style={{ margin: '16px 0' }}>
+          <Breadcrumb.Item>Home</Breadcrumb.Item>
+          <Breadcrumb.Item>List</Breadcrumb.Item>
+          <Breadcrumb.Item>App</Breadcrumb.Item>
+        </Breadcrumb>
+        <div className="site-layout-content" style={{ background: colorBgContainer }}>
+          Content
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  );
+};
+
+export default App;

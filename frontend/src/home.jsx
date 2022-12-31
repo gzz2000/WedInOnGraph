5de12e0e0fc02a21a453f16115ca1574413ef23c
GZZ <gzz_2000@126.com>
2022-12-31 20:54:48 +0800

    follow user list

--- frontend/src/home.jsx
+++ frontend/src/home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Row, Col, Card, Space, Alert, Typography, Button, Input } from 'antd';
+import { Row, Col, Card, Space, Alert, Typography, Button, Input, List } from 'antd';
 import { Link } from "react-router-dom";
-import { UserOutlined, LikeOutlined, LikeFilled, SendOutlined, DeleteOutlined } from '@ant-design/icons';
+import { UserOutlined, LikeOutlined, LikeFilled, SendOutlined, DeleteOutlined, PlusOutlined } from '@ant-design/icons';
 
 const { Title, Paragraph, Text } = Typography;
 const { TextArea } = Input;
@@ -28,7 +28,6 @@ const Home = () => {
             size="middle"
             style={{ width: '100%' }}
           >
-            <Title level={3}>All Posts</Title>
             <Card
               bodyStyle={{ padding: '0px' }}
             >
@@ -52,6 +51,7 @@ const Home = () => {
                 </Col>
               </Row>
             </Card>
+            <Title level={3}>All Posts</Title>
             <Card
               size="small"
               type="inner"
@@ -86,15 +86,32 @@ const Home = () => {
                 This is a test weibo post by another person.
               </Paragraph>
               <div style={{ marginBottom: '-8px', marginLeft: '-8px' }}>
-                <Button type="text" icon={<LikeFilled />} />
+                <Button type="text" icon={<LikeOutlined />} />
                 <Text italic>Be the first one to like this!</Text>
               </div>
             </Card>
           </Space>
         </Col>
         <Col span={8} style={{ padding: '20px', paddingLeft: '0px' }}>
-          <Card title="Person To Follow">
-            Hello
+          <Card title="Person To Follow" bodyStyle={{ padding: '0px' }}>
+            <List
+              itemLayout="horizontal"
+              dataSource={['gzz3', 'gzz4']}
+              renderItem={user =>
+                <List.Item extra={
+                  <Button
+                    type="text"
+                          size="small"
+                          icon={<PlusOutlined />}
+                    >Follow</Button>
+                }>
+                  <div>
+                    <UserOutlined /> <b>{user}</b><br />
+                    Followed by gzz2
+                  </div>
+                </List.Item>
+              }
+            />
           </Card>
         </Col>
       </Row>
